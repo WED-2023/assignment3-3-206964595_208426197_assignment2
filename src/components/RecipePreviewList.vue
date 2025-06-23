71a80538528bdfc0fa21e3aa986e44b0e89c2c80
IdanDuha <idan2haviv@gmail.com>
2025-06-23 17:44:33 +0300

    whats going on

--- src/components/RecipePreviewList.vue
+++ src/components/RecipePreviewList.vue
@@ -56,7 +56,11 @@ export default {
             params: {
               limitLicense: true,
               number: 3,
-              apiKey: 'af93fe3fb3e94d16a993328500f9cce5'
+              //apiKey: 'af93fe3fb3e94d16a993328500f9cce5'
+              apiKey: '8ff080815b204f66a9641b65189504ee'
+
+
+            
             }
           }
         );
